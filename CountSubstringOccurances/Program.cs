e6b70b8cef5e1d126b446643f4c1f6243e4efb8a
Stranger <jmanova83@gmail.com>
2017-06-26 23:47:38 +0300

    1,2,3/4 solved

---
+++ CountSubstringOccurances/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSubstringOccurances
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = Console.ReadLine().ToLower();
+			var word = Console.ReadLine().ToLower();
+			var count = 0;
+			var index = -1;
+			while (true)
+			{
+				index = input.IndexOf(word, index+1);
+				if (index >= 0)
+				{
+					count++;
+				}
+				else
+				{
+					break;
+				}
+			}
+			Console.WriteLine(count);
+		}
+	}
+}
