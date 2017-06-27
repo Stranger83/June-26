5e56cd68e1db7534f6b6118409c8816c494fc607
Stranger <jmanova83@gmail.com>
2017-06-27 22:34:43 +0300

    all done

---
+++ Palindromes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var words = Console.ReadLine()
+				.Split(new char[] { ' ', ',', '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries)
+				.ToList();
+			
+			var palindromes = new List<string>();
+			foreach (var word in words)
+			{
+				var reversed = new string(word.Reverse().ToArray());
+				if (word.Length == 1 || word == reversed)
+				{
+					palindromes.Add(word);
+				}
+			}
+			palindromes = palindromes.Distinct().OrderBy(x => x).ToList();
+			Console.WriteLine(string.Join(", ", palindromes));
+		}
+	}
+}
